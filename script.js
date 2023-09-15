dab855e3c76586323f02a74e9c713fbc3c29c513
KateSebring <ihatesuperman99@gmail.com>
2023-09-15 13:59:48 -0400

    Set up grid square and container for squares

---
+++ script.js
@@ -0,0 +1,7 @@
+const sketchContainer = document.querySelector('.sketch-container');
+
+const squareDiv = document.createElement('div');
+
+squareDiv.setAttribute('class', 'grid-square');
+
+sketchContainer.appendChild(squareDiv);
