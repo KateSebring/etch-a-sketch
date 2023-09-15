4db752edba4123fb2b4dc1eded77c79a9f0982e5
KateSebring <ihatesuperman99@gmail.com>
2023-09-15 15:47:34 -0400

    Default size set to 16x16; add button allowing user to input custom size, up to 100

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
 const sketchContainer = document.querySelector('.sketch-container');
 
-function createGrid() {
-    const squareDiv = document.createElement('div');
-    squareDiv.setAttribute('class', 'grid-square');
-    sketchContainer.appendChild(squareDiv);
-}
+let gridSize = 16;
 
-for (let i = 0; i < 256; i++) {
-    createGrid();
-}
+createGrid();
 
-const squareDiv = document.getElementsByClassName('grid-square');
+const btn = document.querySelector('button');
+btn.addEventListener('click', () => {
+    gridSize = prompt("What size would you like your grid to be?");
+    if (gridSize > 100) {
+        alert("Value cannot be over 100. Please try again.");
+        gridSize = 16;
+    } else {
+        createGrid();
+    }
+});
 
-for (let j = 0; j < squareDiv.length; j++) {
-    squareDiv[j].addEventListener("mouseover", () => {
-        squareDiv[j].style.backgroundColor = "black";
+function addEventListenerToTiles() {
+    const allGrid = document.querySelectorAll('.grid-square');
+
+    allGrid.forEach (element => {
+        element.addEventListener('mouseover', () => {
+            element.style.backgroundColor = 'black'
+        });
     })
+}
+
+function createGrid() {
+    sketchContainer.innerHTML = "";
+    for (let i = 0; i < (gridSize * gridSize); i++) {
+        const squareDiv = document.createElement('div');
+        squareDiv.setAttribute('class', 'grid-square');
+        squareDiv.style.width = 960 / gridSize + 'px';
+        squareDiv.style.height = 960 / gridSize + 'px';
+        sketchContainer.appendChild(squareDiv);
+    }
+    addEventListenerToTiles();
 }
